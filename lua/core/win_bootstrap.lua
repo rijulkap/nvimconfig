e9babdb3928ab8cac70ef91fa6d5d14941f3cfbc
rijulkap <rijulkapoor@gmail.com>
2024-04-11 19:55:27 +1000

    removed powershell crap

--- lua/core/win_bootstrap.lua
+++ lua/core/win_bootstrap.lua
@@ -1,15 +1,6 @@
 if vim.fn.exists 'g:os' == 0 then
   local is_windows = vim.fn.has 'win64' == 1 or vim.fn.has 'win32' == 1 or vim.fn.has 'win16' == 1
   if is_windows then
-    vim.opt.shell = 'pwsh.exe -NoLogo'
-    vim.opt.shellcmdflag =
-      '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
-    vim.cmd [[
-        let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-        let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-        set shellquote= shellxquote=
-      ]]
-
     -- Set a compatible clipboard manager
     vim.g.clipboard = {
       copy = {
