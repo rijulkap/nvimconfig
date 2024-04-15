8faba44d7cfd0bdf1720930dcbf05aab69f172ca
Rijul Kapoor <rijul.kapoor@global.komatsu>
2024-04-16 09:16:43 +1000

    added ts context

--- lua/config.lua
+++ lua/config.lua
@@ -408,6 +408,10 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    opts = {},
+  },
   --ADD LAZY STUFF HERE
   -- require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
