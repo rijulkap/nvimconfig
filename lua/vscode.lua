4c22d4f584c4ef80dc7ae10d8c8cca93374ff811
Rijul Kapoor <rijulkapoor@gmail.com>
2024-04-15 18:07:44 +1000

    fixed weird copying behaviour in vscode

--- lua/vscode.lua
+++ lua/vscode.lua
@@ -1,4 +1,5 @@
 -- Add vscode specific thingys here
+require 'core.win_bootstrap'
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
@@ -6,3 +7,5 @@ vim.keymap.set('n', 'H', '^', { silent = true, noremap = true })
 vim.keymap.set('n', 'L', '$', { silent = true, noremap = true })
 
 vim.keymap.set('n', '<leader>x', '*``cgn', { desc = 'Replace word' })
+
+vim.opt.clipboard = 'unnamedplus'
