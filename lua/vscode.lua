d9510b85b198f52f57ddb272aa51c09d1eff627c
Rijul Kapoor <rijul.kapoor@global.komatsu>
2024-04-17 07:57:42 +1000

    uncommented test code

--- lua/vscode.lua
+++ lua/vscode.lua
@@ -1,11 +1,11 @@
 -- Add vscode specific thingys here
 require 'core.win_bootstrap'
 
+vim.opt.clipboard = 'unnamedplus'
+
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 vim.keymap.set('n', 'H', '^', { silent = true, noremap = true })
 vim.keymap.set('n', 'L', '$', { silent = true, noremap = true })
 
 vim.keymap.set('n', '<leader>x', '*``cgn', { desc = 'Replace word' })
-
-vim.opt.clipboard = 'unnamedplus'
