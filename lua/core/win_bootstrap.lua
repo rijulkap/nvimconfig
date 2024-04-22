df618c3431351d2020e5019d7e3c79309b5615f3
Rijul Kapoor <rijul.kapoor@global.komatsu>
2024-04-22 10:01:53 +1000

    fixed toggleter powershell

--- lua/core/win_bootstrap.lua
+++ lua/core/win_bootstrap.lua
@@ -2,6 +2,19 @@ if vim.fn.exists 'g:os' == 0 then
   local is_windows = vim.fn.has 'win64' == 1 or vim.fn.has 'win32' == 1 or vim.fn.has 'win16' == 1
   if is_windows then
     -- Set a compatible clipboard manager
+    local powershell_options = {
+      shell = vim.fn.executable 'pwsh' == 1 and 'pwsh' or 'powershell',
+      shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;',
+      shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait',
+      shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode',
+      shellquote = '',
+      shellxquote = '',
+    }
+
+    for option, value in pairs(powershell_options) do
+      vim.opt[option] = value
+    end
+
     vim.g.clipboard = {
       copy = {
         ['+'] = 'win32yank.exe -i --crlf',
