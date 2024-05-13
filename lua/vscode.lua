f5c38e835a43bb2a826ed0509794e863c32ec302
Rijul Kapoor <rijul.kapoor@global.komatsu>
2024-05-13 13:41:45 +1000

    added telescope buffer change

--- lua/vscode.lua
+++ lua/vscode.lua
@@ -9,3 +9,9 @@ vim.keymap.set('n', 'H', '^', { silent = true, noremap = true })
 vim.keymap.set('n', 'L', '$', { silent = true, noremap = true })
 
 vim.keymap.set('n', '<leader>x', '*``cgn', { desc = 'Replace word' })
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
