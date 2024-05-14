c71fcad931589ff96c85e41453b449e4be4cfff7
Rijul Kapoor <rijul.kapoor@global.komatsu>
2024-05-14 10:29:01 +1000

    more telescope buffer changes

--- lua/config.lua
+++ lua/config.lua
@@ -141,7 +141,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', function()
-        builtin.buffers { sort_mru = true }
+        builtin.buffers { sort_mru = true, ignore_current_buffer = true }
       end, { desc = 'Find existing buffers' })
 
       vim.keymap.set('n', '<leader>s/', function()
