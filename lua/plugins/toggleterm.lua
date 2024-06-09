94a90bcf985b5f75e8b38dbd7131f5d9228a871b
rijulkap <rijulkapoor@gmail.com>
2024-06-09 20:28:51 +1000

    incomplete toggleterm setup

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,12 +3,12 @@ return {
     'akinsho/toggleterm.nvim',
     version = '*',
     opts = {
-      size = 20,
+      size = 60,
       open_mapping = [[<c-\>]],
       autochdir = true,
       hide_numbers = true,
       -- persist_mode = true,
-      direction = 'float',
+      -- direction = 'float',
       close_on_exit = true,
       shell = vim.o.shell,
       auto_scroll = true,
@@ -19,5 +19,28 @@ return {
         enabled = false,
       },
     },
+    config = function(_, opts)
+      require('toggleterm').setup(opts)
+      local Terminal = require('toggleterm.terminal').Terminal
+      local bot = Terminal:new { direction = 'horizontal' }
+      local side = Terminal:new { direction = 'vertical' }
+      local float = Terminal:new { direction = 'float' }
+
+      vim.keymap.set('n', '<A-1>', function()
+        bot:toggle()
+      end)
+
+      vim.keymap.set('n', '<A-2>', function()
+        side:toggle()
+      end)
+
+      vim.keymap.set('n', '<A-3>', function()
+        float:toggle()
+      end)
+
+      vim.keymap.set('t', '<esc><esc>', function()
+        vim.cmd ':ToggleTerm'
+      end)
+    end,
   },
 }
