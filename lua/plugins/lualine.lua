8b66a9538ec956fabeba60d603a4d47599473d74
Rijul Kapoor <rijul.kapoor@global.komatsu>
2024-06-10 16:46:33 +1000

    fixed lualine bug

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -76,8 +76,7 @@ return {
           },
         },
         lualine_y = {
-          { 'progress', separator = ' ', padding = { left = 1, right = 0 } },
-          { 'location', padding = { left = 0, right = 1 } },
+          { 'progress', separator = ' ', padding = { left = 1, right = 1 } },
         },
         lualine_z = { 'location' },
       },
