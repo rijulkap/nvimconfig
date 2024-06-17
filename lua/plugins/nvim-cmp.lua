eeed18fc188200239838ca4d0afae802ae36edce
Rijul Kapoor <rijul.kapoor@global.komatsu>
2024-06-18 08:22:57 +1000

    Changed cmp mappings

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -15,11 +15,11 @@ return {
         completion = { completeopt = 'menu,menuone,noinsert,noselect' },
         preselect = cmp.PreselectMode.None,
         mapping = cmp.mapping.preset.insert {
-          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<Tab>'] = cmp.mapping.select_next_item(),
           ['<C-p>'] = cmp.mapping.select_prev_item(),
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<CR>'] = cmp.mapping.confirm { select = true },
           ['<C-Space>'] = cmp.mapping.complete {},
         },
         sources = {
